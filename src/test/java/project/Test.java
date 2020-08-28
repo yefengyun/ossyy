a035ff9dd136fc4adaf1b09fded3baf8a0ceefff
Administrator <Administrator@WINDOWS-LCT7DHF>
2020-08-28 09:50:54 +0800

    构建跳过test
    Change-Id: I0000000000000000000000000000000000000007
    Signed-off-by: Administrator <Administrator@WINDOWS-LCT7DHF>

--- src/test/java/project/Test.java
+++ src/test/java/project/Test.java
@@ -1,28 +1,28 @@
-//package project;
-//
-//
-//import org.junit.Assert;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import com.tong.ossyy.Application;
-//import com.tong.ossyy.controller.UserController;
-//import com.tong.ossyy.util.ResultJson;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes=Application.class)
-//public class Test  {
-//	@Autowired
-//    private UserController user;
-//
-//    @org.junit.Test
-//    public void getHello() throws Exception {
-//        final ResultJson<Object> rs = user.insert();
-//        System.out.println(rs.getMsg());
-//        Assert.assertEquals("SUCCESS", rs.getMsg());
-//    }
-//
-// 
-//}
+package project;
+
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tong.ossyy.Application;
+import com.tong.ossyy.controller.UserController;
+import com.tong.ossyy.util.ResultJson;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class)
+public class Test  {
+	@Autowired
+    private UserController user;
+
+    @org.junit.Test
+    public void getHello() throws Exception {
+        final ResultJson<Object> rs = user.insert();
+        System.out.println(rs.getMsg());
+        Assert.assertEquals("SUCCESS", rs.getMsg());
+    }
+
+ 
+}
